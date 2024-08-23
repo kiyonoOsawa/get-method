b634612145e9eeb14af7830142ebc5b1161b6655
KiyonoOsawa <kiyo.sntr.x6@icloud.com>
2024-08-24 02:55:54 +0900

    追加だんだん

--- app.rb
+++ app.rb
@@ -3,3 +3,21 @@ Bundler.require
 require 'sinatra/reloader' if development?
 
 # ここにコードを書こう
+
+get '/' do
+    erb :index
+end
+
+get '/answer/yes' do
+    @message = 'すごい！正解です！！'
+    @is_correct = true
+    @bilson_img = '/get-method/public/img/maru_big.png'
+    erb :answer
+end
+
+get '/answer/no' do
+    @message = 'ざんねん！！もう一回チャレンジしてみよう！'
+    @is_correct = false
+    @bilson_img = '/get-method/public/img/maru_big.png'
+    erb :answer
+end
